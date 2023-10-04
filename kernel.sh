fa4f685f6df7cceaa41a558f58f383758b87ef4b
Ali シャ <76146196+Asyanx@users.noreply.github.com>
2023-10-04 16:08:21 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -52,7 +52,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="sea-dewi-t1"
+ZIPNAME="sea-dewi-t2"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
