d653a5e3fbd31dd39333e93de2ae5640e8b2c620
Ali シャ <76146196+Asyanx@users.noreply.github.com>
2023-10-26 07:09:57 +0700

    Switch latest clang zyc

--- kernel.sh
+++ kernel.sh
@@ -180,8 +180,8 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 	if [ $COMPILER = "clang" ]
 	then
                 mkdir clang-llvm
-		wget https://github.com/ZyCromerZ/Clang/releases/download/18.0.0-20230813-release/Clang-18.0.0-20230813.tar.gz -O "Clang-18.0.0-20230813.tar.gz"
-                tar -xf Clang-18.0.0-20230813.tar.gz -C clang-llvm
+		wget https://github.com/ZyCromerZ/Clang/releases/download/18.0.0-20231025-release/Clang-18.0.0-20231025.tar.gz -O "Clang-18.0.0-20231025.tar.gz"
+                tar -xf Clang-18.0.0-20231025.tar.gz -C clang-llvm
 		git clone https://github.com/ZyCromerZ/aarch64-zyc-linux-gnu -b 14 gcc64 --depth=1
                 git clone https://github.com/ZyCromerZ/arm-zyc-linux-gnueabi -b 14 gcc32 --depth=1
 		GCC64_DIR=$KERNEL_DIR/gcc64
