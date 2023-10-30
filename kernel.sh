c2d92fe48dd83f6be17ea6a3a9334ffc4ac074a5
Ali シャ <76146196+Asyanx@users.noreply.github.com>
2023-10-30 22:53:55 +0700

    Update add & enable `KCAL`

--- kernel.sh
+++ kernel.sh
@@ -54,7 +54,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="sea-WhoCare-T5-KSU"
+ZIPNAME="sea-WhoCare-T5-KCAL-KSU"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
