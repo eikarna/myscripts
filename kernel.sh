74ba39897009f1490a7658b7137f7ab0457a58a2
Ali シャ <76146196+Asyanx@users.noreply.github.com>
2023-12-26 18:13:08 +0700

    T6

--- kernel.sh
+++ kernel.sh
@@ -24,7 +24,7 @@ KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
 git clone --single-branch --depth=1 https://github.com/Asyanx/Sea_Kernel-Fog.git -b test $KERNEL && cd $KERNEL
-export LOCALVERSION=1/EireneTC2🐲✨
+export LOCALVERSION=1/Eirene🐲✨
 
 # Bail out if script fails
 set -e
@@ -54,7 +54,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="sea-Eirene-T5a-KSU-CASS"
+ZIPNAME="sea-Eirene-T6-KSU"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
