d7eb4a385e81d6c98fded9b889a11735a4f32048
Ali シャ <76146196+Asyanx@users.noreply.github.com>
2023-12-28 15:31:52 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -54,7 +54,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="sea-Eirene-T6t1-KSU"
+ZIPNAME="sea-Eirene-T6t2-KSU"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
