4d019a7ce5c5a11cb30d5cd9f0b24da2e78b4239
Ali シャ <76146196+Asyanx@users.noreply.github.com>
2024-03-27 19:47:06 +0700

    NONKSU

--- kernel.sh
+++ kernel.sh
@@ -23,7 +23,8 @@ WORKDIR="$(pwd)"
 KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
-git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b fog-r-oss $KERNEL && cd $KERNEL
+git clone --single-branch https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b fog-r-oss $KERNEL && cd $KERNEL
+git reset --hard ef9b908a2ed78c1cbedb6e9d91a8ee051876947b
 export LOCALVERSION=🐉/Uclamp
 
 # Bail out if script fails
@@ -54,7 +55,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="SeaWE-KSU-UClamp-T4"
+ZIPNAME="SeaWE-KSU-UClamp-T3-NONKSU"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
@@ -108,7 +109,7 @@ FILES=Image.gz
 BUILD_DTBO=0
 
 # PATCH KERNELSU
-KSU=1
+KSU=0
 if [ $KSU = 1 ]
 then
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
