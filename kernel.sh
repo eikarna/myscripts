6361cf24caeefd5c2ad77f9f5424cb695b513c0a
Ali シャ <76146196+Asyanx@users.noreply.github.com>
2024-03-27 11:51:36 +0700

    T4

--- kernel.sh
+++ kernel.sh
@@ -54,7 +54,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="SeaWE-KSU-UClamp-T3"
+ZIPNAME="SeaWE-KSU-UClamp-T4"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
