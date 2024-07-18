7ae1d2d224cee2558065030260f5e9b59bd5c8c9
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2024-07-18 10:09:36 +0700

    R Whale

--- kernel.sh
+++ kernel.sh
@@ -24,7 +24,7 @@ KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
 git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b fog-r-oss-staging $KERNEL && cd $KERNEL
-export LOCALVERSION=🦈/shark
+export LOCALVERSION=🐋/whale
 
 # Bail out if script fails
 set -e
@@ -74,7 +74,7 @@ fi
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
 AUTHOR="Asyanx"
-HOSTR="#FunProject"
+HOSTR="fogkernel"
 
 # Architecture
 ARCH=arm64
@@ -188,8 +188,8 @@ WAKTU=$(date +"%F-%S")
 	if [ $COMPILER = "clang" ]
 	then
                 mkdir clang-llvm
-		wget https://github.com/ZyCromerZ/Clang/releases/download/19.0.0git-20240615-release/Clang-19.0.0git-20240615.tar.gz -O "Clang-19.0.0git-20240615.tar.gz"
-                tar -xf Clang-19.0.0git-20240615.tar.gz -C clang-llvm
+		wget https://github.com/ZyCromerZ/Clang/releases/download/19.0.0git-20240717-release/Clang-19.0.0git-20240717.tar.gz -O "Clang-19.0.0git-20240717.tar.gz"
+                tar -xf Clang-19.0.0git-20240717.tar.gz -C clang-llvm
 		git clone https://github.com/ZyCromerZ/aarch64-zyc-linux-gnu -b 14 gcc64 --depth=1
                 git clone https://github.com/ZyCromerZ/arm-zyc-linux-gnueabi -b 14 gcc32 --depth=1
 		GCC64_DIR=$KERNEL_DIR/gcc64
