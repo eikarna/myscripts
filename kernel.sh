c14f44ed164d76c3b4ed125a1d7d73b863cbf60f
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2024-08-19 20:31:35 +0700

    SeaWE `🦕dino` | KSU

--- kernel.sh
+++ kernel.sh
@@ -23,8 +23,8 @@ WORKDIR="$(pwd)"
 KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
-git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b luna-staging $KERNEL && cd $KERNEL
-export LOCALVERSION=✨🦕
+git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b fog-r-oss-staging $KERNEL && cd $KERNEL
+export LOCALVERSION=🦕/dino
 
 # Bail out if script fails
 set -e
@@ -55,7 +55,7 @@ BASEDIR="$(basename "$KERNEL_DIR")"
 
 # PATCH KERNELSU & RELEASE VERSION
 KSU=1
-release=T2
+release=R2s
 if [ $KSU = 1 ]
 then
 	echo "CONFIG_SUS_FS=y" >> arch/arm64/configs/vendor/"fog-perf_defconfig"
@@ -67,15 +67,15 @@ fi
 # The name of the Kernel, to name the ZIP
 if [ $KSU = 1 ]
 then
-   ZIPNAME="Luna-KSU"
+   ZIPNAME="SeaWE-KSU"
 else
-    ZIPNAME="Luna-NONKSU"
+    ZIPNAME="SeaWE-NONKSU"
 fi
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
 AUTHOR="Asyanx"
-HOSTR="XLuNax"
+HOSTR="XSeAWe"
 
 # Architecture
 ARCH=arm64
