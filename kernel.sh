cffce2217b68747bd61fb967e0b00c7314b9a999
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2024-09-02 20:22:24 +0700

    T2.5 KSU | `✨🦄`
    update latest clang

--- kernel.sh
+++ kernel.sh
@@ -24,7 +24,7 @@ KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
 git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b luna-staging $KERNEL && cd $KERNEL
-export LOCALVERSION=✨🦕
+export LOCALVERSION=✨🦄
 
 # Bail out if script fails
 set -e
@@ -54,8 +54,8 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # PATCH KERNELSU & RELEASE VERSION
-KSU=0
-release=T2
+KSU=1
+release=T2s
 if [ $KSU = 1 ]
 then
 	echo "CONFIG_SUS_FS=y" >> arch/arm64/configs/vendor/"fog-perf_defconfig"
@@ -189,8 +189,8 @@ WAKTU=$(date +"%F-%S")
 	if [ $COMPILER = "clang" ]
 	then
                 mkdir clang-llvm
-		wget https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20240811-release/Clang-20.0.0git-20240811.tar.gz -O "Clang-20.0.0git-20240811.tar.gz"
-                tar -xf Clang-20.0.0git-20240811.tar.gz -C clang-llvm
+		wget https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20240902-release/Clang-20.0.0git-20240902.tar.gz -O "Clang-20.0.0git-20240902.tar.gz"
+                tar -xf Clang-20.0.0git-20240902.tar.gz -C clang-llvm
 		git clone https://github.com/ZyCromerZ/aarch64-zyc-linux-gnu -b 14 gcc64 --depth=1
                 git clone https://github.com/ZyCromerZ/arm-zyc-linux-gnueabi -b 14 gcc32 --depth=1
 		GCC64_DIR=$KERNEL_DIR/gcc64
