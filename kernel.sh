f8d334369669e596de265009ec2722bab29b17ce
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2024-09-29 07:45:37 +0700

    Bucin

--- kernel.sh
+++ kernel.sh
@@ -23,8 +23,8 @@ WORKDIR="$(pwd)"
 KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
-git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b fog-r-oss-staging $KERNEL && cd $KERNEL
-export LOCALVERSION=🦕/dino
+git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b bucin-kila $KERNEL && cd $KERNEL
+export LOCALVERSION=✨😍
 
 # Bail out if script fails
 set -e
@@ -55,7 +55,7 @@ BASEDIR="$(basename "$KERNEL_DIR")"
 
 # PATCH KERNELSU & RELEASE VERSION
 KSU=1
-release=R2s
+release=B1
 if [ $KSU = 1 ]
 then
 	echo "CONFIG_SUS_FS=y" >> arch/arm64/configs/vendor/"fog-perf_defconfig"
@@ -67,15 +67,15 @@ fi
 # The name of the Kernel, to name the ZIP
 if [ $KSU = 1 ]
 then
-   ZIPNAME="SeaWE-KSU"
+   ZIPNAME="Bucin-KSU"
 else
-    ZIPNAME="SeaWE-NONKSU"
+    ZIPNAME="Bucin-NONKSU"
 fi
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
 AUTHOR="Asyanx"
-HOSTR="XSeAWe"
+HOSTR="AlixKila"
 
 # Architecture
 ARCH=arm64
