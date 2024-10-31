88f2c0bf033fe2cebed1edf5d2ed4c926e6abec5
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2024-11-01 00:19:16 +0700

    Rename Bucin to KhususTest

--- kernel.sh
+++ kernel.sh
@@ -24,7 +24,7 @@ KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
 git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b bucin-kila $KERNEL && cd $KERNEL
-export LOCALVERSION=✨🐣
+export LOCALVERSION=👑🫀✨
 
 # Bail out if script fails
 set -e
@@ -54,8 +54,8 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # PATCH KERNELSU & RELEASE VERSION
-KSU=0
-release=T3
+KSU=1
+release=B2
 if [ $KSU = 1 ]
 then
 	echo "CONFIG_SUS_FS=y" >> arch/arm64/configs/vendor/"fog-perf_defconfig"
@@ -67,15 +67,15 @@ fi
 # The name of the Kernel, to name the ZIP
 if [ $KSU = 1 ]
 then
-   ZIPNAME="Luna-KSU"
+   ZIPNAME="KhususTest-KSU"
 else
-    ZIPNAME="Luna-NONKSU"
+    ZIPNAME="KhususTest-NONKSU"
 fi
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
 AUTHOR="Asyanx"
-HOSTR="xLuNaX"
+HOSTR="Kila"
 
 # Architecture
 ARCH=arm64
